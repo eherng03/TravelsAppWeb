78d763bb1e9a6fe3b4923e2fec932df485621194
EvaHergar <eherng03@estudiantes.unileon.es>
2017-05-30 11:48:37 +0200

    version with background

---
+++ logic/initWindowLogic.js
@@ -0,0 +1,24 @@
+$(document).ready(function($) {
+	$(".form-container").hide();
+	$(".login-container").hide();
+	$(".register-container").hide();
+
+	$(".login-button").click(function(event) {
+		if ($(".login-container").is(":hidden")){
+			$(".login-container").slideDown();
+			$(".register-container").slideUp();
+		}else{
+			$(".login-container").slideUp();
+		}
+	});
+
+	$(".register-button").click(function(event) {
+		if ($(".register-container").is(":hidden")){
+			$(".register-container").slideDown();
+			$(".login-container").slideUp();
+		}else{
+			$(".register-container").slideUp();
+		}
+	});
+
+});
