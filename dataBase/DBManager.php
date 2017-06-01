9fccc941990d3289c2e355e898c246d801d73834
Iker Núñez Rodríguez <drdomingas@gmail.com>
2017-06-01 22:34:01 +0200

    Register and login with sessions

--- dataBase/DBManager.php
+++ dataBase/DBManager.php
@@ -1,29 +1,35 @@
 <?php
 	class DBManager{
 		private static $instance;
-		private static $connection;
+		private $connection;
 		
 		private $host = 'sql11.freemysqlhosting.net';
 		private $user = 'sql11176712';
 		private $pass = 'xKr66u4JUh';
 		private $db = 'sql11176712';
 		private $conn = null;
+		
 		private function __construct(){
 			$this->openConnectionDB();
-			$this->testDB();
 		}
+		
 		public static function getInstance(){
-			if (  !self::$instance instanceof self){
+			if (!self::$instance instanceof self){
 				self::$instance = new self;
 			}
 			return self::$instance;
 		}
+		
 		function openConnectionDB(){
-			$this->instance = new mysqli($this->host,$this->user,$this->pass);
-			if($this->instance->connect_errno){
-			  die('Connection error' . $this->instance->connect_error);
+			$this->connection = new mysqli($this->host,$this->user,$this->pass);
+			if($this->connection->connect_errno){
+			  die('Connection error' . $this->connection->connect_error);
 			}
-			$this->instance->select_db($this->db);
+			$this->connection->select_db($this->db);
+		}
+		
+		public function getConnection(){
+			return $this->connection;
 		}
 		
 	}
---
+++ models/registerControl.php
@@ -0,0 +1,40 @@
+<?php
+	require "../dataBase/DBManager.php";
+
+	$singleton = DBManager::getInstance();
+	$conn = $singleton->getConnection();
+	
+	$userType = 0;
+	if($_POST['optradio'] === "driver")
+		$userType = 1;
+	
+	$name = $_POST['nameRegister'];
+	$user = $_POST['usernameRegister'];
+	$pass = $_POST['passwordRegister'];
+	$dni = $_POST['dniRegister'];
+	$email = $_POST['emailRegister'];
+	$phone = $_POST['phoneRegister'];
+	
+	//Image
+	$target_dir = "../userImages/";
+	$target_file = $target_dir.basename($_FILES['userImage']['name']);
+	$imageType = pathinfo($target_file, PATHINFO_EXTENSION);
+	move_uploaded_file($_FILES['userImage']['tmp_name'], $target_dir.$user.".".$imageType);
+
+	$file = $user.'.'.$imageType;
+	
+	$query = $conn->prepare("INSERT INTO Users(username, pass, name, dni, email, phone, photo, rol) VALUES (?,?,?,?,?,?,?,?)");
+	$query->bind_param("sssssssi", $user,$pass,$name,$dni,$email,$phone,$file,$userType);
+	
+	if($query->execute()){ 
+		$query->close();
+		print_r("Registro completo. Redirigiendo a la pagina principal.");
+		sleep(2);
+		header("Location: ../graphic/initWindow.html");
+	} else {
+		$query->close();
+		print_r("Error en el registro. Redirigiendo a la pagina principal.");
+		sleep(2);
+		header("Location: ../graphic/initWindow.html");
+	}
+?>
