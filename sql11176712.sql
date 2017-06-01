21f31e81b002a03a34d588946ec4cd83e478c23f
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-06-01 23:28:39 +0200

    Iker changes register and login with sessions

--- sql11176712.sql
+++ sql11176712.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: sql11.freemysqlhosting.net
--- Tiempo de generación: 27-05-2017 a las 00:05:48
+-- Tiempo de generación: 01-06-2017 a las 11:36:44
 -- Versión del servidor: 5.5.53-0ubuntu0.14.04.1
 -- Versión de PHP: 5.3.28
 
@@ -23,13 +23,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Destinations`
+-- Estructura de tabla para la tabla `Chats`
 --
 
-CREATE TABLE IF NOT EXISTS `Destinations` (
-  `city` varchar(15) NOT NULL,
-  `price` int(11) NOT NULL,
-  `arrivalTime` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `Chats` (
+  `user1` int(11) NOT NULL,
+  `user2` int(11) NOT NULL,
+  `hour` varchar(10) NOT NULL,
+  `msg` varchar(200) NOT NULL COMMENT '200 char max',
+  PRIMARY KEY (`user1`,`user2`,`hour`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -39,31 +41,11 @@ CREATE TABLE IF NOT EXISTS `Destinations` (
 --
 
 CREATE TABLE IF NOT EXISTS `Drivers` (
-  `name` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `user` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `telephone` varchar(9) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `photo` varchar(1000) NOT NULL COMMENT 'Photo = image src in localhost',
-  `averageScore` int(11) NOT NULL,
-  PRIMARY KEY (`dni`),
-  UNIQUE KEY `dni` (`dni`),
-  UNIQUE KEY `dni_2` (`dni`),
-  UNIQUE KEY `dni_3` (`dni`)
+  `username` varchar(30) NOT NULL COMMENT 'foreign key',
+  `score` int(11) NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `DriversJourneys`
---
-
-CREATE TABLE IF NOT EXISTS `DriversJourneys` (
-  `driverDni` int(11) NOT NULL,
-  `journeyId` int(11) NOT NULL,
-  PRIMARY KEY (`driverDni`,`journeyId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Links each driver with its journeys';
 
 -- --------------------------------------------------------
 
@@ -72,34 +54,15 @@ CREATE TABLE IF NOT EXISTS `DriversJourneys` (
 --
 
 CREATE TABLE IF NOT EXISTS `Journeys` (
-  `source` int(11) NOT NULL,
-  `initDate` date NOT NULL,
-  `seatsNumber` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Each journey is composed of 1 or more destinations';
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `JourneysDestinations`
---
-
-CREATE TABLE IF NOT EXISTS `JourneysDestinations` (
-  `journeyId` int(11) NOT NULL,
-  `destinationId` int(11) NOT NULL,
-  PRIMARY KEY (`journeyId`,`destinationId`)
+  `tripID` int(11) NOT NULL COMMENT 'foreign key',
+  `journeyID` int(11) NOT NULL,
+  `hour` varchar(5) NOT NULL COMMENT 'format: "##:##"',
+  `cost` int(11) NOT NULL,
+  `nSeats` int(11) NOT NULL,
+  `origin` varchar(40) NOT NULL,
+  `destination` varchar(40) NOT NULL,
+  PRIMARY KEY (`tripID`,`journeyID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `JourneysPassengers`
---
-
-CREATE TABLE IF NOT EXISTS `JourneysPassengers` (
-  `journeyId` int(11) NOT NULL,
-  `passengerDni` int(11) NOT NULL,
-  PRIMARY KEY (`journeyId`,`passengerDni`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Links each journey with its passengers';
 
 -- --------------------------------------------------------
 
@@ -108,16 +71,45 @@ CREATE TABLE IF NOT EXISTS `JourneysPassengers` (
 --
 
 CREATE TABLE IF NOT EXISTS `Passengers` (
+  `username` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Trips`
+--
+
+CREATE TABLE IF NOT EXISTS `Trips` (
+  `tripID` int(11) NOT NULL AUTO_INCREMENT,
+  `driverUsername` varchar(30) NOT NULL COMMENT 'foreign key',
+  `origin` varchar(40) NOT NULL,
+  `destination` varchar(40) NOT NULL,
+  `totalAmount` int(11) NOT NULL,
+  PRIMARY KEY (`tripID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `username` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `dni` varchar(9) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `user` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `telephone` varchar(9) NOT NULL,
-  `dni` int(9) NOT NULL,
-  `photo` varchar(1000) NOT NULL COMMENT 'Photo = image src in localhost',
-  PRIMARY KEY (`dni`),
-  UNIQUE KEY `dni` (`dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Each passenger can have several journeys linked with it';
+  `phone` varchar(9) NOT NULL,
+  `photo` varchar(1000) NOT NULL COMMENT 'src path to img',
+  `rol` int(11) NOT NULL COMMENT '0 passenger / 1 driver',
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `dni` (`dni`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
