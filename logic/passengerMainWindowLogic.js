607e81cea1d713e000e2ba4b0a3f5e58454e1cad
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-03 16:55:55 +0200

    Passenger updated

--- logic/passengerMainWindowLogic.js
+++ logic/passengerMainWindowLogic.js
@@ -1,11 +1,27 @@
-$(document).ready(function($) {
-	$(".journeys-container").hide();
+$(document).ready(function() {
+	$(".search-container").hide();
 
 	$(".journeys-button").click(function(event) {
+		//Si pulso en ver trayectos se esconden las demás opciones
 		if ($(".journeys-container").is(":hidden")){
 			$(".journeys-container").slideDown();
-		}else{
+			$(".search-container").slideUp();
+		}
+	});
+
+	$(".search-button").click(function(event) {
+		if ($(".search-container").is(":hidden")){
+			$(".search-container").slideDown();
 			$(".journeys-container").slideUp();
 		}
+	});
+
+	$('.input-group').datepicker({
+	    format: "dd/mm/yyyy",
+	    todayBtn: "linked",
+	    language: "es",
+	    daysOfWeekHighlighted: "0,6",
+	    autoclose: true,
+	    todayHighlight: true
 	});
 });
