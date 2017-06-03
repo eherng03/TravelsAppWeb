4833efaa3719025a26b7dd7757a506bec6cb07d4
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-03 16:09:31 +0200

    Driver modified

--- logic/driverMainWindowLogic.js
+++ logic/driverMainWindowLogic.js
@@ -1,13 +1,13 @@
 $(document).ready(function($) {
-	$(".journeys-container").hide();
+	$(".create-container").hide();
 	$(".score-container").hide();
 
 	$(".journeys-button").click(function(event) {
+		//Si pulso en ver trayectos se esconden las demás opciones
 		if ($(".journeys-container").is(":hidden")){
 			$(".journeys-container").slideDown();
 			$(".score-container").slideUp();
-		}else{
-			$(".journeys-container").slideUp();
+			$(".create-container").slideUp();
 		}
 	});
 
@@ -15,9 +15,25 @@ $(document).ready(function($) {
 		if ($(".score-container").is(":hidden")){
 			$(".score-container").slideDown();
 			$(".journeys-container").slideUp();
-		}else{
+			$(".create-container").slideUp();
+		}
+	});
+
+	$(".create-button").click(function(event) {
+		if ($(".create-container").is(":hidden")){
+			$(".create-container").slideDown();
+			$(".journeys-container").slideUp();
 			$(".score-container").slideUp();
 		}
+	});
+
+	$('.input-group').datepicker({
+	    format: "dd/mm/yyyy",
+	    todayBtn: "linked",
+	    language: "es",
+	    daysOfWeekHighlighted: "0,6",
+	    autoclose: true,
+	    todayHighlight: true
 	});
 
 });
