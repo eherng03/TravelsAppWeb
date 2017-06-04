7c50ea42077a4e168fc61c0fd881e22d54fc29ef
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-06-04 17:50:30 +0200

    verification with json

--- operations/verificationNumber.php
+++ operations/verificationNumber.php
@@ -1,4 +1,4 @@
 <?php  
 	$digits = 5;
-	echo str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+	echo json_encode(str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT));
 ?>
