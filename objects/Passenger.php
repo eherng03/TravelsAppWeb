024bfee74a1fab306c176439d04087a0f49e98fb
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-04 12:04:34 +0200

    Chat update. Objects updated

--- objects/Passenger.php
+++ objects/Passenger.php
@@ -1,19 +1,19 @@
 <?php
 	class Passenger{
-		private $name;
-		private $email;
-		private $user;
-		private $password;
-		private $telephone;
-		private $dni;
-		private $photo;
-		private $journeys;
+		public $name;
+		public $email;
+		public $user;
+		public $password;
+		public $telephone;
+		public $dni;
+		public $photo;
+		public $journeys;
 
-		public function __construct($name, $email, $user, $password, $telephone, $dni, $photo){
+		public function __construct($name, $email, $user, $telephone, $dni, $photo){
 			$this->name = $name;
 			$this->email = $email;
 			$this->user = $user;
-			$this->password = $password;
+			//$this->password = $password;
 			$this->telephone = $telephone;
 			$this->dni = $dni;
 			$this->photo = $photo;
