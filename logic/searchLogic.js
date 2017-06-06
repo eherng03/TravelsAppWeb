56b33db9e5e067ca477a53efb73f9274951cd02e
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-06 10:35:39 +0200

    Seach logic. Only shows destinations and origins

---
+++ logic/searchLogic.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	//Rellena el select de origenes
+	$.get("../operations/selectJourneySearch.php", function(data) {
+        var comboBox = document.getElementById("Origin");
+        var journeys = JSON.parse(data);
+
+        journeys.forEach((journey) =>{
+            var opt = document.createElement('option');
+            //TODO funciona porque origen es privado
+            opt.innerHTML = journey.origin;
+            comboBox.appendChild(opt);
+        });
+    });
+
+    //Rellena el select de destinos
+	$.get("../operations/selectJourneySearch.php", function(data) {
+        var comboBox = document.getElementById("Destiny");
+        var journeys = JSON.parse(data);
+
+        journeys.forEach((journey) =>{
+            var opt = document.createElement('option');
+            //TODO funciona porque origen es privado
+            opt.innerHTML = journey.destination;
+            comboBox.appendChild(opt);
+        });
+    });
+});
