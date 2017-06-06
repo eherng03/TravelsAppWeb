f9ec94f3eefdc80604b76e01898b0660678432ca
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-06 19:16:25 +0200

    Update Journey.php

--- objects/Journey.php
+++ objects/Journey.php
@@ -1,26 +1,32 @@
 <?php
 //TRAYECTO
 	class Journey{ 
-		//private $tripID;??	Hay que saber a que viaje pertenece de alguna manera para obteer el conductor
-		private $source;
-		private $destinations;
-		private $initDate;
+		private $tripID;		//??	Hay que saber a que viaje pertenece de alguna manera para obteer el conductor
+		private $journeyID;
+		public $origin;
+		public $destination;
+		private $departureHour;
+		private $departureDate;
+		private $arrivaleHour;
+		private $arrivalDate;
 		private $seatsNumber;
 		private $passengers;
 		private $cost;
 
-		public function __construct($source, $destinations, $initDate, $seatsNumber, $cost){
-			$this->source = $source;
-			$this->destinations = $destinations;
-			$this->initDate = $initDate;
+		public function __construct($tripID, $journeyID, $departureHour, $departureDate, $arrivaleHour, $arrivalDate, $origin, $destination, $seatsNumber, $cost){
+			$this->tripID = $tripID;
+			$this->journeyID = $journeyID;
+			$this->origin = $origin;
+			$this->destination = $destination;
+			$this->departureHour = $departureHour;
+			$this->departureDate = $departureHour;
+			$this->arrivaleHour = $departureHour;
+			$this->arrivalDate = $departureHour;
 			$this->seatsNumber = $seatsNumber;
 			$this->passengers = array();
 			$this->cost = $cost;
 		}
 
-		//TODO here needed getters and setters
-
-		//TODO addPassenger and check the number of free sits
 
 	}
 ?>
