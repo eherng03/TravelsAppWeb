40f3f1b3256f3cf06e1847c8d550c4bd6f6f63fd
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-07 12:56:33 +0200

    Sesion control updated

--- operations/sessionControl.php
+++ operations/sessionControl.php
@@ -1,44 +1,49 @@
 <?php
 	require "../dataBase/DBManager.php";
 	session_start();
-
+	
 	$singleton = DBManager::getInstance();
 	$conn = $singleton->getConnection();
 	
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
+	
+	if($user == "admin" && $pass == "admin"){
+		header("Location: ../graphic/adminMainWindow.php");
+	}else{
+			
+		$query = $conn->prepare("SELECT * FROM Users WHERE username = ?");
+		$query->bind_param("s", $user);
 
-	$query = $conn->prepare("SELECT * FROM Users WHERE username = ?");
-	$query->bind_param("s", $user);
-	
-	$query->execute();
-	$result = $query->get_result();
-	
-	if($result->num_rows > 0){
-		$userData = $result->fetch_array();
-		
-		if($pass === $userData['pass']){
+		$query->execute();
+		$result = $query->get_result();
+
+		if($result->num_rows > 0){
+			$userData = $result->fetch_array();
+			
+			if($pass === $userData['pass']){
+				$query->close();
+				
+				$_SESSION['username'] = $user;
+				$_SESSION['isLogged'] = true;
+				$_SESSION['userType'] = $userData['rol'];
+				
+				if($userData['rol'] === 0){
+					header("Location: ../graphic/passengerMainWindow.php"); 
+					exit();
+				}
+				else{
+					header("Location: ../graphic/driverMainWindow.php");
+					exit();
+				}
+			
+			} else {
+				print_r("Contraseña incorrecta.");
+			}
+		}else {
 			$query->close();
-			
-			$_SESSION['username'] = $user;
-			$_SESSION['isLogged'] = true;
-			$_SESSION['userType'] = $userData['rol'];
-			
-			if($userData['rol'] === 0)
-				header("Location: ../graphic/passengerMainWindow.html"); 
-			else header("Location: ../graphic/driverMainWindow.html"); 
-			
-			//$_SESSION['start'] = time();
-			//$_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
-		
-		} else {
-			print_r("Contraseña incorrecta.");
+			header("Location: ../graphic/initWindow.php?login=error"); 
+			exit();
 		}
-	}else {
-		$query->close();
-		print_r("Usuario y/o contraseña incorrectos. Redirigiendo a la pagina principal.");
-		sleep(2);
-		header("Location: ../graphic/initWindow.html"); 
-		
 	}
 ?>
