ab57f90abb774aee56bda881fa4780916cc38686
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-08 21:57:55 +0200

    Add files via upload

--- objects/Journey.php
+++ objects/Journey.php
@@ -5,23 +5,19 @@
 		private $journeyID;
 		public $origin;
 		public $destination;
-		private $departureHour;
 		private $departureDate;
-		private $arrivaleHour;
 		private $arrivalDate;
 		private $seatsNumber;
 		private $passengers;
 		private $cost;
 
-		public function __construct($tripID, $journeyID, $departureHour, $departureDate, $arrivaleHour, $arrivalDate, $origin, $destination, $seatsNumber, $cost){
+		public function __construct($tripID, $journeyID, $departureDate, $arrivalDate, $origin, $destination, $seatsNumber, $cost){
 			$this->tripID = $tripID;
 			$this->journeyID = $journeyID;
 			$this->origin = $origin;
 			$this->destination = $destination;
-			$this->departureHour = $departureHour;
-			$this->departureDate = $departureHour;
-			$this->arrivaleHour = $departureHour;
-			$this->arrivalDate = $departureHour;
+			$this->departureDate = $departureDate;			
+			$this->arrivalDate = $arrivalDate;
 			$this->seatsNumber = $seatsNumber;
 			$this->passengers = array();
 			$this->cost = $cost;
--- objects/Trip.php
+++ objects/Trip.php
@@ -7,16 +7,19 @@
 		private $price;
 		private $journeys;
 		private $initDate;
-		private $initHour;
 
-		public function __construct($origin, $destination, $driverUsername, $price){
+		public function __construct($origin, $destination, $driverUsername){
 			$this->origin = $origin;
 			$this->destination = $destination;
 			$this->driverUsername = $driverUsername;
 			$this->journeys = array();
-			$this->price = $price;
+			$this->price = 0;
 		}
 
 		//TODO here needed getters and setters
+		function addJourney($journey){
+			array_push($this->journeys, $journey);
+			
+		}
 	}
 ?>
