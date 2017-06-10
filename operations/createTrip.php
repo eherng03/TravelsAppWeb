c64c6c0e243be55bd27f20bee8e2b9d4368d335c
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-10 16:59:37 +0200

    Create journeys and dests

--- operations/createTrip.php
+++ operations/createTrip.php
@@ -1,10 +1,52 @@
 <?php
-    include '../dataBase/DBManager.php';
+    include_once '../dataBase/DBManager.php';
+    include_once "../objects/Journey.php";
+    include_once "../models/TripControl.php";
+    include_once "../models/JourneyControl.php";
 	
     //acceso a la BBDD
     $singleton = DBManager::getInstance();
     $conn = $singleton -> getConnection();
 	
-	print_r($_POST);
+	$origin = $_POST['origin'];
+	$dateOrigin = strtotime($_POST['dateOrigin']);
+	$nDests = $_POST['nDests'];
+	$driver = $_POST['driver'];
+
+	$journeyOrigin = $origin;			//el primer origen del journey es el del viaje
+	$departureDate = $dateOrigin;		//la primera fecha de salida de los journeys es la de inicio
+	$journeys = array();
+
+	$journeyDest = "";
+	for ($i = 1; $i <= $nDests; $i++) {
+    	$destString = "dest".$i; 
+    	$journeyDest = $_POST[$destString];		//Destino i
+    	$nSeatsString = "nSeats".$i; 
+    	$nSeats = $_POST[$nSeatsString];		//numero de asientos del journey i
+    	$arrivalDateString = "date".$i; 
+    	$arrivalDate = strtotime($_POST[$arrivalDateString]);		//numero de asientos del journey i
+    	$priceString = "price".$i;
+    	$price = $_POST[$priceString];
+
+    	array_push($journeys, new Journey('0', $i, $departureDate, $arrivalDate, $journeyOrigin, $journeyDest, $nSeats, $price));
+    	$departureDate = $arrivalDate;
+    	$journeyOrigin = $journeyDest;
+	}
+	//En la ultima vuelta del bucle se queda guardado en $journeyDest, el ultimo destino
+	
+	$tripControl = TripControl::getInstance();	
+	$resultTrip = $tripControl->insertTrip($driver, $origin, $journeyDest);		//Trip insertado
+	$tripID = "";
+	while ($row = $resultTrip->fetch_array()){
+        $tripID = $row['tripID'];
+    }
+
+	foreach ($journeys as $jour) {
+		$journeyControl = JourneyControl::getInstance();
+		$resultJourney = $journeyControl->insertJourney($tripID, $jour->getID() , $jour->getDepartureDate(), $jour->getArrivalDate(), $jour->getPrice(), $jour->getSeats(), $jour->getOrigin(), $jour->getDestination());	
+	}
+
+
+	print_r("Viaje insertado correctamente.");
 
 ?>
