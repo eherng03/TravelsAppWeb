a3e49e06836abe5b21dc6badec7da22efbadd825
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-06-10 19:59:36 +0200

    book well

--- operations/book.php
+++ operations/book.php
@@ -3,15 +3,9 @@
 
 	$idTrip = $_POST['idTrip'];
 	$idsJourneyString = $_POST['idsJourney'];
-	//$idTrip = 1;
-	//$idsJourneyString = "1 2 ";
 	$idsJourney = split("[ ]+", $idsJourneyString);
 	array_pop($idsJourney);
 	$username = $_POST['username'];
-	//$username = "alba";
-	print_r($username);
-	print_r($idsJourney);
-	print_r($idTrip);
 	$journeyPassengersControl = JourneyPassengersControl::getInstance();
 	foreach ($idsJourney as $idJourney) {
 		$journeyPassengersControl->insertPassenger($idTrip, $idJourney, $username);
