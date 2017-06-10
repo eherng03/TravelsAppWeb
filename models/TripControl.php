5f5a704fee2717dc5b08a34e55292bcb176c1230
EvaHergar <eherng03@estudiantes.unileon.es>
2017-06-10 17:01:31 +0200

    Insert trip done

--- models/TripControl.php
+++ models/TripControl.php
@@ -21,5 +21,15 @@
 			$query = $connection->query("SELECT driverUsername FROM trips WHERE tripID = '$tripID'");
 			return $query;
 		}
+		
+		function insertTrip($driverID, $origin, $destination){
+			$dbManager = DBManager::getInstance();
+			$connection = $dbManager->getConnection();
+			$query = $connection->query("INSERT INTO trips(driverUsername, origin, destination) VALUES ('$driverID' ,'$origin','$destination')");
+			
+			$result = $connection->query("SELECT tripID FROM trips WHERE (driverUsername = '$driverID') AND (origin = '$origin') AND  (destination = '$destination')");
+
+			return $result;		//Devuelve el ultimo elemento (creo)
+		}
 	}
 ?>
