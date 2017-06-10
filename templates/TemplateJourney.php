d67ff1cf4cf653758b6bd2a573d5ea533585c378
Iker Núñez Rodríguez <drdomingas@gmail.com>
2017-06-10 11:45:57 +0200

    cambios alba + iker

--- templates/TemplateJourney.php
+++ templates/TemplateJourney.php
@@ -1,6 +1,7 @@
 <?php
 
 	class TemplateJourney{
+
 		private static $instance;
 		private function __construct(){}
 		
@@ -11,7 +12,7 @@
 			return self::$instance;
 		}
 		
-		public function getTemplate($driver, $trip, $userNameLogged){
+		public function getTemplate($driver, $journey, $userNameLogged){
 			
 		   	$html = "<div class = 'Journey'> 
 						<table>
@@ -32,6 +33,7 @@
 							  <td>Día y hora de salida: ".$trip->getInitDate()." Día y hora de llegada: ".$trip->getArrivalDate()."</td>
 							</tr>	
 						</table>";
+						
 			if($userNameLogged != '-'){
 				$journeysID = array();
 				$journeys = $trip->getJourneys();
@@ -41,11 +43,13 @@
 				$html .= "<input class='book-button' type='button' value='Reservar' id = 'bookBtn' onclick = 'bookClicked()''>
 					<input type='hidden' id='journeyID' value=".json_encode($journeysID)."/>";
 			}
+			
 			$html .= "</div>";
 		   	return $html;	
 		}
 
 		public function getTemplateForDriver($journey, $trip){
+			
 		   	return "<div class = 'Journey'> 
 						<table>
 							<tr>
