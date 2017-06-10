a87b4d302b8c59ed0990c6fd993be10baad99143
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-06-10 23:50:13 +0200

    unbook with 6h before requirement

--- operations/unbook.php
+++ operations/unbook.php
@@ -1,10 +1,23 @@
 <?php
 	include "../models/JourneyPassengersControl.php";
-	echo "primero";
+	include "../models/JourneyControl.php";
 	$idTrip = $_POST['idTrip'];
 	$idJourney = $_POST['idJourney'];
 	$username = $_POST['username'];
-	$journeyPassengersControl = JourneyPassengersControl::getInstance();
-	$journeyPassengersControl->cancelJourney($idTrip, $idJourney, $username);
-	print_r("llega");
+
+	$journeyControl = JourneyControl::getInstance();
+	$journey = $journeyControl->getJourneyDatabyJourneyAndTrip($idJourney, $idTrip);
+	while($rowJourney = $journey->fetch_array()){
+		$date = $rowJourney['departureDate'];
+	}
+	$currentDate = $milliseconds = round(microtime(true));
+
+	if(($date - $currentDate) > 216000){ //se puede cancelar hasta 6 horas antes del viaje
+		$journeyPassengersControl = JourneyPassengersControl::getInstance();
+		$journeyPassengersControl->cancelJourney($idTrip, $idJourney, $username);
+		echo "Ha cancelado su trayecto";
+	}else{
+		echo "Es demasiado tarde para cancelar su trayecto";
+	}
+
 ?>
